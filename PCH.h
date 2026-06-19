88b66ee8bc9fe2247410054a6261fc9c6e647994
adammantium44-jpg <adammantium44@gmail.com>
2026-06-19 21:49:27 +0700

    Update PCH.h

--- PCH.h
+++ PCH.h
@@ -1,28 +1,9 @@
 #pragma once
 
-#ifndef WIN32_LEAN_AND_MEAN
-    #define WIN32_LEAN_AND_MEAN
-#endif
+// This file is required.
 
-#ifndef NOMINMAX
-    #define NOMINMAX
-#endif
-
-#include <Windows.h>
-#include <cstdint>
-#include <string>
-#include <filesystem>
-#include <fstream>
-#include <sstream>
-#include <cstring>
-
-// CommonLibSSE NG (baru, bukan CommonLibSSE lama)
-#include <CommonLibSSE/PluginAPI.h>
-#include <CommonLibSSE/SKSE.h>
-#include <CommonLibSSE/Logging.h>
-#include <CommonLibSSE/RE/Skyrim.h>
-
-// PrismaUI
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
 #include "PrismaUI_API.h"
 
 // Global variables
@@ -53,3 +34,5 @@ void AdminConfig(const char* result);
 void InitializeHUD();
 void InitializeConfig();
 void UpdateHUDStats();
+
+using namespace std::literals;
